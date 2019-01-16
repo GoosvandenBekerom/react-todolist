0966c2cde80c631495a137e91fc3c36ecde3bf6b
Goos van den Bekerom <goos.bekerom@gmail.com>
2019-01-16 17:07:26 +0100

    Changed class specifications to className

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom';
 
 const Header = () => (
     <header style={{marginBottom: '15px'}}>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark text-uppercase">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark text-uppercase">
 
         <Link className="navbar-brand" to="/">
           React todo list
         </Link>
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div className="collapse navbar-collapse" id="navbarNav">
 
-          <ul class="navbar-nav">
-            <li class="nav-item active">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
               <Link className="nav-link" to="/">Home</Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link className="nav-link" to="/about">About</Link>
             </li>
           </ul>
